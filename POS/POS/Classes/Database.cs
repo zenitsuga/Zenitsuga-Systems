dcb5e56ae70145dbe8856b15d08444dba7a52fa3
zenitsuga <bok.zenitsuga@gmail.com>
2018-09-06 09:53:55 +0800

    Update the Customer Module
    Upload the Customer Query

---
+++ POS/POS/Classes/Database.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace POS.Classes
+{
+    public class Database
+    {
+        public string DBPath;
+        public string SQLConnString;
+
+        public bool testConnection(string ConnectionString, ref string ErrMsg)
+        {
+            bool result = false;
+            try
+            {
+                //string sqlConstr = "Data Source=" + DbPath + ";Version=3;New=True;Compress=True;"; 
+                SQLConnString = ConnectionString;
+                DBPath = ConnectionString;
+                string sqlConstr = DBPath; //SQLConnBuilder(DBPath);//SQLConnBuilder(ConnectionString);
+                //SQLiteConnection sconn = new SQLiteConnection(sqlConstr);
+                SqlConnection sconn = new SqlConnection(sqlConstr);
+                sconn.Open();
+                result = sconn.State == System.Data.ConnectionState.Open ? true : false;
+            }
+            catch (Exception ex)
+            {
+                ErrMsg = ex.Message.ToString();
+            }
+            return result;
+        }
+        public bool ExecuteNonQuery(string SQLStatement)
+        {
+            bool result = false;
+            try
+            {
+                //string SqlConnstr = SQLConnBuilder(DBPath);
+                string sqlConstr = SQLConnString;//SQLConnBuilder(DBPath);
+                SqlConnection sconn = new SqlConnection(sqlConstr);
+                sconn.Open();
+                if (sconn.State == ConnectionState.Open)
+                {
+                    SqlCommand sqlcom = new SqlCommand(SQLStatement, sconn);
+                    sqlcom.ExecuteReader();
+                    result = true;
+                }
+            }
+            catch
+            {
+            }
+            return result;
+        }
+        public DataTable ExecuteQuery(string SQLStatement)
+        {
+            DataTable dtResult = new DataTable();
+            try
+            {
+                string SqlConnstr = SQLConnString; //SQLConnBuilder(DBPath);
+                SqlConnection sconn = new SqlConnection(SqlConnstr);
+                sconn.Open();
+                if (sconn.State == ConnectionState.Open)
+                {
+                    SqlDataAdapter sda = new SqlDataAdapter(SQLStatement, sconn);
+                    sda.Fill(dtResult);
+                }
+            }
+            catch
+            {
+            }
+            return dtResult;
+        }
+    }
+}
---
+++ POS/POS/GetRecords.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using POS.Classes;
+namespace POS
+{
+    public partial class GetRecords : Form
+    {
+        public string ConnectionString;
+        public string TableName;
+        public string FieldNames;
+        public string Criteria;
+        public string Orderby;
+        public DataGridViewRow ResultOutput;
+        
+        string QueryStatement = string.Empty;
+
+        DataTable dtRecords;
+
+        Database db = new Database();
+
+        public GetRecords()
+        {
+            InitializeComponent();
+        }
+
+        private void GetRecords_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dtRecords = new DataTable();
+                db.DBPath = ConnectionString;
+                db.SQLConnString = db.DBPath;
+                QueryStatement = QueryBuilder(TableName, FieldNames, Criteria); 
+                dtRecords = db.ExecuteQuery(QueryStatement);
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = dtRecords;
+                dataGridView1.Refresh();
+            }
+            catch
+            {
+            }
+        }
+        private string QueryBuilder(string tablename, string FieldName, string Criteria)
+        {
+            string result = string.Empty;
+            try
+            {
+                result = "Select " + FieldName + " from " + tablename + (string.IsNullOrEmpty(Criteria) ? "" : " where " + Criteria);
+            }
+            catch
+            {
+            }
+            return result;
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                ResultOutput = dataGridView1.SelectedRows[0];
+                this.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    ResultOutput = dataGridView1.SelectedRows[0];
+                    this.Close();
+                }
+            }
+        }
+
+        private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                ResultOutput = dataGridView1.SelectedRows[0];
+                this.Close();
+            }
+        }
+    }
+}
