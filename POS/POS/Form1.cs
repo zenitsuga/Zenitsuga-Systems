728448fb1f7369a73b6baa556fddc3a5aa44396e
zenitsuga <bok.zenitsuga@gmail.com>
2018-12-06 09:37:21 +0800

    Revert "add Customer Query/Select"
    This reverts commit ab876059765c9d334c0fc074bdd014348a89453f.

--- POS/POS/Form1.cs
+++ POS/POS/Form1.cs
@@ -7,17 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using POS.Classes;
-using System.Reflection;
-using System.IO;
+
 namespace POS
 {
     public partial class Form1 : Form
     {
-        clsFunction cf = new clsFunction();
-        bool isLocalConnected = false;
-        bool isRemoteConnected = false;
-
         public Form1()
         {
             InitializeComponent();
@@ -26,54 +20,6 @@ namespace POS
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
-        }
-
-        private void setupToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            frmConfigSettings cs = new frmConfigSettings();
-            cs.ShowDialog();
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            tssDate.Text = "Date : " + DateTime.Now.ToString("MM-dd-yyyy hh:mm:ss");
-            isLocalConnected  = (cf.checkDBConnection("Local") ? true : false);
-            tssDBLocalStatus.Text = "Local: " + (isLocalConnected == true ? "Connected":"Disconnected");
-        }
-
-        private void FocusBarcode()
-        {
-            tbBarcode.Focus();
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {   
-            if (!cf.isAppValid())
-            {
-                Application.Exit();
-                return;
-            }
-            FocusBarcode();
-        }
-
-        private void customerInquiryToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                GetRecords gr = new GetRecords();
-                gr.ConnectionString = cf.ConnectionStringBuilder("LocalServer");
-                gr.TableName = "tblCustomer";
-                gr.FieldNames = "sysID,LastName,FirstName,Address,ContactNumber";
-                gr.Criteria = " isActive =1";
-                gr.ShowDialog();
-                if (gr.ResultOutput != null)
-                {
-                    string CustomerName = gr.ResultOutput.Cells["LastName"].Value.ToString() + "," + gr.ResultOutput.Cells["Biagtan"].Value.ToString(); 
-                    tbCustormerName.Text = CustomerName;
-                }
-            }
-            catch
-            {
-            }
         }
     }
 }
